222d6d3e63b0b1175431abe2b2d2617678eb7ddd
jlampley <jlample5@vols.utk.edu>
2016-04-21 15:08:08 -0400

    new

--- April15open2.R
+++ April15open2.R
@@ -1,4 +1,5 @@
- #April12016practice
+#April12016practice
 #plot (phy)
+#PhyloMethSpring2016.R is my command file and April15open2.R is my run file
 
 source("C:\\Users\\JAL\\Desktop\\PhyloMeth\\PhyloSpring2016\\PhyloMethSpring2016.R")
