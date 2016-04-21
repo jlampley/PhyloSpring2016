fbfaeec7fb601d88d1018087db19d95b805d2d82
jlampley <jlample5@vols.utk.edu>
2016-04-21 15:42:42 -0400

    new again

--- April15open2.R
+++ April15open2.R
@@ -3,3 +3,9 @@
 #PhyloMethSpring2016.R is my command file and April15open2.R is my run file
 
 source("C:\\Users\\JAL\\Desktop\\PhyloMeth\\PhyloSpring2016\\PhyloMethSpring2016.R")
+my_melan_book <- read.csv ("C:\\Users\\JAL\\Desktop\\Melanthiaceae_book.csv", stringsAsFactors = FALSE, header = FALSE)
+my_melan_book <- my_melan_book[,c(1:3)]
+my_names <- rep(NA, dim(my_melan_book)[1])
+for (i in sequence(length(my_names))) {
+  my_names[i] <- paste(my_melan_book[i,], collapse="_")
+}
--- PhyloMethSpring2016.R
+++ PhyloMethSpring2016.R
@@ -1,9 +1,8 @@
-my_melan_book <- read.csv ("C:\\Users\\JAL\\Desktop\\Melanthiaceae_book.csv", stringsAsFactors = FALSE, header = FALSE)
-my_melan_book <- my_melan_book[,c(1:3)]
-my_names <- rep(NA, dim(my_melan_book)[1])
-for (i in sequence(length(my_names))) {
-  my_names[i] <- paste(my_melan_book[i,], collapse="_")
-}
+#this is my command file, run file is April15open2.R
+library(phytools)
+library(ape)
+library(geiger)
+
 nreps = 1000
 all.counts <- data.frame()
 for (i in sequence(nreps)) {
